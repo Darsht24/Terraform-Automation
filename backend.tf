b58a476059140eed1193f259cbd3fa96b75e5d31
Darsht24 <151422529+Darsht24@users.noreply.github.com>
2024-01-06 15:22:30 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "s3-bucket-for-terraform"
-    key = "main"
+    key = "Linux_Pass"
     region = "us-east-1"
     dynamodb_table = "s3-bucket-for-terraform"
   }
